2a2b8a2cf78ac57d76387453f77d8770ad259429
Jordan Kniest <jkniest@Jordans-MacBook-Pro.local>
2017-05-25 21:01:36 +0200

    Added simple header with navbar

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+    <title>{{config('app.name')}}</title>
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+</head>
+<body>
+    @yield('main')
+</body>
