c26f58ec0049dace75920b82d85b41b136592aad
Jordan Kniest <jkniest@gena-clusters.net>
2017-06-18 03:26:53 +0000

    Added project: "Planet of the Elements"

---
+++ resources/views/projects/partials/en/PlanetoftheElements.blade.php
@@ -0,0 +1,106 @@
+@php
+    /**
+     * Copyright (C) 2017 Jordan Kniest
+     *
+     * This program is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU Affero General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * This program is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     * GNU Affero General Public License for more details.
+     *
+     * You should have received a copy of the GNU Affero General Public License
+     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
+@endphp
+
+
+<section>
+    <p>
+        The mini-game "Planet of the Elements" was developed for a game competition within 50 hours by two people.
+        The player has the goal to kill enemies appering in waves without dying himself.
+    </p>
+    <br>
+    <p>
+        These enemies drop coins that the player can use in shops to buy random items and weapons. There are almost
+        20 different items and five different weapons.
+    </p>
+</section>
+
+<hr class="m3t">
+
+<section class="m3t">
+    <h3 class="title is-3">Features</h3>
+
+    <section class="m2t">
+
+        <h4 class="title is-4">Random items</h4>
+
+        <p>
+            There are almost 20 different items, all of which can be combined and interacted with. Some of the items
+            must be used manually, while others have a continuous effect, such as attribute increases (speed, damage,
+            etc.) or additional effects such as fire damage.
+        </p>
+
+    </section>
+
+    <section class="m3t">
+
+        <h4 class="title is-4">Enemys / Waves</h4>
+
+        <p>
+            A total of three enemies with completely different behaviors randomly spawn in waves in the world. One
+            enemy is inactive until the players come near to it. Another enemy makes poison damage. Each wave is
+            more difficult than the previous one, because on the one hand the enemies become stronger, but also more
+            enemies appear.
+        </p>
+
+    </section>
+
+    <section class="m3t">
+
+        <h4 class="title is-4">Shops / Chests</h4>
+
+        <p>
+            Within the level are some shops and chests. For a certain amount, you can buy random items or weapons
+            there.
+        </p>
+
+    </section>
+
+</section>
+
+<hr class="m3t">
+
+<section class="m3t">
+
+    <h3 class="title is-3">Development</h3>
+
+    <p>
+        The game was developed with the <a href="https://unity3d.com">Unity3D Engine</a>. The game can be found on
+        <a href="http://gamejolt.com/games/planet-of-the-elements/53528">Gamejolt</a>. There is a web, windows, mac
+        and linux version.
+    </p>
+    <br>
+    <p>
+        I was responsible for the complete programming. The graphics were done by Tobias Daniel, a friend of mine.
+        A total of almost 50 hours have been spent on the project.
+    </p>
+
+</section>
+
+<hr class="m3t">
+
+<section class="m3t">
+
+    <h3 class="title is-3">Conclusion</h3>
+
+    <p>
+        The development of this game was a lot of fun. We also came to the second round of the competition, but for
+        reasons of time we had to cancel the project and leave the competition.
+    </p>
+
+</section>
---
+++ resources/views/projects/partials/en/PlanetoftheElements_short.blade.php
@@ -0,0 +1,20 @@
+@php
+    /**
+     * Copyright (C) 2017 Jordan Kniest
+     *
+     * This program is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU Affero General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * This program is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     * GNU Affero General Public License for more details.
+     *
+     * You should have received a copy of the GNU Affero General Public License
+     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
+@endphp
+
+Planet of the Elements is a Unity3D mini-game, which was developed within 50 hours for a game competition.
