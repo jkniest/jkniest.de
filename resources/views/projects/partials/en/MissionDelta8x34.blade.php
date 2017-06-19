3b97057effeb702d324d273e62e2c5c6f638526d
Jordan Kniest <jkniest@gena-clusters.net>
2017-06-19 00:57:17 +0000

    Added project: 'Mission: Delta-8x34'

---
+++ resources/views/projects/partials/en/MissionDelta8x34.blade.php
@@ -0,0 +1,91 @@
+@php
+    /**
+     * Copyright (C) 2017 Jordan Kniest
+     *
+     * This program is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU Affero General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * This program is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     * GNU Affero General Public License for more details.
+     *
+     * You should have received a copy of the GNU Affero General Public License
+     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
+@endphp
+
+
+<section>
+    <p>
+        The video game "Mission: Delta-8x34" was developed for the Ludum-Dare Competition. Goal at the Ludum-Dare is
+        to create a video game within 48 hours, according to a specific theme. The topic will be published at the start
+        of the 48 hours. This time it was: "Small worlds".
+    </p>
+    <br>
+    <p>
+        The player slips into the role of a space engineer. He has the task of repairing a military satellite on a
+        "micro-world" since it has not sent any signals for several days.
+    </p>
+</section>
+
+<hr class="m3t">
+
+<section class="m3t">
+    <h3 class="title is-3">Features</h3>
+
+    <section class="m2t">
+
+        <h4 class="title is-4">Controls</h4>
+
+        <p>
+            The micro planet has its own gravitation. The player can go completely around the planet.
+        </p>
+
+    </section>
+
+    <section class="m3t">
+
+        <h4 class="title is-4">Animations</h4>
+
+        <p>
+            The spaceship flies completely animated on the micro planet and back.
+        </p>
+
+    </section>
+
+</section>
+
+<hr class="m3t">
+
+<section class="m3t">
+
+    <h3 class="title is-3">Development</h3>
+
+    <p>
+        The game was developed with the <a href="https://unity3d.com">Unity3D Engine</a>. The game can be found on
+        <a href="http://gamejolt.com/games/mission-delta8x34/252040">Gamejolt</a>. There is a Windows-, Mac- and
+        Linux-Version available.
+    </p>
+    <br>
+    <p>
+        The game was developed and modeled within 48 hours. I have done the programming. The models were created by
+        Markus Kapp, a friend of mine.
+    </p>
+
+</section>
+
+<hr class="m3t">
+
+<section class="m3t">
+
+    <h3 class="title is-3">Conclusion</h3>
+
+    <p>
+        I think this is my first game, which feels complete and ready. There are soundeffects, a clear goal, several
+        missions and an end. Add to that a simple story. I also like the art style very well.
+    </p>
+
+</section>
---
+++ resources/views/projects/partials/en/MissionDelta8x34_short.blade.php
@@ -0,0 +1,20 @@
+@php
+    /**
+     * Copyright (C) 2017 Jordan Kniest
+     *
+     * This program is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU Affero General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * This program is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     * GNU Affero General Public License for more details.
+     *
+     * You should have received a copy of the GNU Affero General Public License
+     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
+@endphp
+
+Mission: Delta-8x34 is a mini game. It was developed within 48 hours for the Ludum-Dare competition.
