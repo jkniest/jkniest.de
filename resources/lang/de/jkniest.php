994de7e20e746424276c1484ebf65eea847f7d43
Jordan Kniest <mail@jkniest.de>
2018-01-13 19:54:11 +0100

    fix(Layout): Fixed typo in contact / about section (Platformenen -> Platformen)

--- resources/lang/de/jkniest.php
+++ resources/lang/de/jkniest.php
@@ -1,30 +1,47 @@
 <?php
 
+/**
+ * Copyright (C) 2018 Jordan Kniest
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 return [
 
-    'iam'             => 'Hi, ich bin',
-    'projects'        => 'Projekte',
-    'about-me'        => 'Über mich',
-    'contact'         => 'Kontakt aufnehmen',
+    'iam' => 'Hi, ich bin',
+    'projects' => 'Projekte',
+    'about-me' => 'Über mich',
+    'contact' => 'Kontakt aufnehmen',
     'english-version' => 'English version',
 
-    'welcome'            => 'Herzlich Willkommen',
+    'welcome' => 'Herzlich Willkommen',
     'short-introduction' => 'Auf der folgenden Seite können Sie einige meiner Arbeiten und Projekte finden.',
 
     'some-projects' => 'Hier finden Sie einige meiner Projekte',
-    'get-more'      => 'Projekt-Details ansehen',
+    'get-more' => 'Projekt-Details ansehen',
 
     'overview' => 'Zurück zur Übersicht',
 
     'project-info' => 'Projekt Informationen',
-    'year'         => 'Erscheinungsjahr',
+    'year' => 'Erscheinungsjahr',
 
     'get-in-touch' => 'Kontakt aufnehmen',
 
-    'by'       => 'von',
+    'by' => 'von',
     'licensed' => 'Quellcode und Inhalte lizensiert unter der',
-    'imprint'  => 'Impressum',
-    'privacy'  => 'Datenschutz Informationen',
+    'imprint' => 'Impressum',
+    'privacy' => 'Datenschutz Informationen',
 
     'introduction' => [
         'welcome' => '
@@ -50,16 +67,16 @@ return [
 
         'contact-me' => 'Dann kontaktieren Sie mich doch einfach:',
 
-        'active' => 'Folgend finden Sie einige Platformenen auf denen ich aktiv bin.',
+        'active' => 'Folgend finden Sie einige Platformen auf denen ich aktiv bin.',
 
         'happy' => 'Ich freue mich auf Ihre Nachricht!',
     ],
 
     'meta' => [
-        'framework'           => 'Framework',
-        'languages'           => 'Sprachen',
-        'license'             => 'Lizenz',
-        'engine'              => 'Engine',
+        'framework' => 'Framework',
+        'languages' => 'Sprachen',
+        'license' => 'Lizenz',
+        'engine' => 'Engine',
         'development-pattern' => 'Entwicklungs-Pattern',
     ],
 ];
